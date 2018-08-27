5e2bd7b77aa241fd553c9b591ec9d5ab7cb73fb0
Andrew Gardhouse <andrewgardhouse@macbook-air.gateway.2wire.net>
2018-08-27 16:08:14 -0400

    Adds timer option opening button to App

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,7 @@
 <template>
   <div id="app">
-    <h1>Just Breathe</h1>
-    <img alt="Vue logo" src="./assets/logo.png">
-    <button class="btn btn-outline">Options</button>
+    <p class="h1">Just Breathe</p>
+    <button class="btn btn-outline open-options" v-on:click="openOptions">Options</button>
     <Timer />
     <TimerOptions />
   </div>
@@ -18,16 +17,24 @@ export default {
     Timer,
     TimerOptions,
   },
+  methods: {
+    openOptions() {
+      this.$modal.show('timer-options');
+    }
+  }
 };
 </script>
 
 <style lang="less">
+body {
+  background-color: #faf5ef;
+}
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
