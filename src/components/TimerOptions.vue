57f3cac3af1859527b55e88255be7bd5156f8854
Andrew Gardhouse <andrewgardhouse@macbook-air.gateway.2wire.net>
2018-08-27 14:30:17 -0400

    Adds Timer and TimerOptions components and removes example HelloWorld component

---
+++ src/components/TimerOptions.vue
@@ -0,0 +1,15 @@
+<template>
+  <modal name="timer-options">
+    <div class="timer-options">
+      <p class="h1">Timer Options</p>
+    </div>
+  </modal>
+</template>
+
+<script>
+export default {
+};
+</script>
+
+<style lang="less">
+</style>
