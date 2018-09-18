4fffd90de0fa3e64ac3780f0e9b92ee582fd10bd
Andrew Gardhouse <andrewgardhouse@macbook-air.gateway.2wire.net>
2018-09-18 12:46:36 -0400

    Adds vuex-persistedstate plugin

--- src/store.js
+++ src/store.js
@@ -1,5 +1,6 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import createPersistedState from 'vuex-persistedstate';
 import state from '@/store/state';
 import getters from '@/store/getters';
 import mutations from '@/store/mutations';
@@ -12,4 +13,5 @@ export default new Vuex.Store({
   getters,
   mutations,
   actions,
+  plugins: [createPersistedState()],
 });
