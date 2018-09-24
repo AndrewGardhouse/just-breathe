b92c60b6083e4793289b7feaf8dd104008844e9d
Andrew Gardhouse <andrewgardhouse@macbook-air.gateway.2wire.net>
2018-09-24 15:03:30 -0400

    Adds test for Timer component clearHoldInhale()

--- tests/unit/Timer.spec.js
+++ tests/unit/Timer.spec.js
@@ -124,6 +124,32 @@ describe('Timer.vue', () => {
     expect(wrapper.vm.holdInhaleCount).toBe(1);
   });
 
+  it('clearHoldInhale sets holdInhaleCount to 0 and clears holdInhaleInterval', () => {
+    const store = new Vuex.Store({
+      state: {
+        inhale: 5,
+        exhale: 5,
+        holdInhale: 5,
+        holdExhale: 5,
+      },
+      getters,
+    });
+
+    const wrapper = mount(Timer, {
+      store,
+      localVue,
+    });
+
+    wrapper.vm.startHoldInhaleCount();
+
+    expect(wrapper.vm.inhaleInterval).toBeDefined();
+
+    wrapper.vm.clearHoldInhale();
+
+    expect(wrapper.vm.holdInhaleInterval).toBeNull();
+    expect(wrapper.vm.holdInhaleCount).toBe(0);
+  });
+
   it('when the countDown is done, the correct methods should be called', () => {
     const store = new Vuex.Store({
       state: {
