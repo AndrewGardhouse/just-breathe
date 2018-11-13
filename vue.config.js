f89596ab3fa09bafad297546cada25345f0e8631
Andrew Gardhouse <AndrewGardhouse@gmail.com>
2018-11-13 14:44:23 -0500

    Updates manifest.json

---
+++ vue.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  pwa: {
+    display: 'fullscreen',
+    background_color: '#fffbf7',
+    theme_color: '#fffbf7',
+    iconPaths: {
+      favicon32: 'img/icons/favicon-32x32.png',
+      favicon16: 'img/icons/favicon-16x16.png',
+      appleTouchIcon: 'img/icons/apple-touch-icon-152x152.png',
+      msTileImage: 'img/icons/msapplication-icon-144x144.png'
+    }
+  }
+}
