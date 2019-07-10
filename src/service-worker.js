feb775f808ada0185c030f393145ddb8b04f73df
AndrewGardhouse <AndrewGardhouse@gmail.com>
2019-07-10 14:49:15 -0400

    Updates service-worker.js

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,33 +1,38 @@
-self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
-workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
-workbox.core.setCacheNameDetails({prefix: "just-breathe"});
+/* eslint-disable no-undef */
+/* eslint-disable no-restricted-globals */
+workbox.setConfig({
+  debug: false,
+});
 
-workbox.precaching.precache([
-  {
-    url: 'https://fonts.googleapis.com/css?family=Comfortaa:300,400,700|Roboto:100,300,400,500,700,900',
-    revision: 'abcd',
-  },
-  {
-    url: 'https://unpkg.com/basscss@8.0.2/css/basscss.min.css',
-    revision: 'abcd',
-  },
-  {
-    url: 'https://unpkg.com/basscss-btn@1.1.0/css/btn.css',
-    revision: 'abcd',
-  },
-  {
-    url: 'https://unpkg.com/basscss-btn-outline@1.1.0/css/btn-outline.css',
-    revision: 'abcd',
-  },
-]);
+workbox.precaching.precacheAndRoute([]);
 
-// Add Precache Route
-workbox.precaching.addRoute();
+workbox.routing.registerRoute(
+  new RegExp('https://unpkg.com/basscss'),
+  workbox.strategies.staleWhileRevalidate({
+    cacheName: 'basscss',
+    plugins: [
+      new workbox.expiration.Plugin({
+        maxEntries: 60,
+      }),
+    ],
+  }),
+);
+
+workbox.routing.registerRoute(
+  new RegExp('https://fonts.(?:googleapis|gstatic).com/(.*)'),
+  workbox.strategies.cacheFirst({
+    cacheName: 'googleapis',
+    plugins: [
+      new workbox.expiration.Plugin({
+        maxEntries: 30,
+      }),
+    ],
+  }),
+);
 
 // install new service worker when ok, then reload page.
 self.addEventListener('message', (msg) => {
-  if (msg.data.action == 'skipWaiting') {
+  if (msg.data.action === 'skipWaiting') {
     self.skipWaiting();
   }
 });
